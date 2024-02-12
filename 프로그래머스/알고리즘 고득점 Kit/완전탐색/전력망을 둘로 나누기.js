3b887c75e316ca3ae40a615c66635b3ce89fd5d3
Pridesd <whwotjr98@naver.com>
2024-02-13 00:05:27 +0900

    Prog/Lv.2/미측정/전력망을 둘로 나누기-완전 탐색

--- 프로그래머스/알고리즘 고득점 Kit/완전탐색/전력망을 둘로 나누기.js
+++ 프로그래머스/알고리즘 고득점 Kit/완전탐색/전력망을 둘로 나누기.js
@@ -31,10 +31,13 @@ function solution(n, wires) {
     );
   });
 
-  return answer === n ? n - 1 : answer;
+  return answer;
 }
 
 /**
  * 풀이는 이해했으나 from과 fill의 차이를 알아야겠다.
  * fill일땐 작동을 안 하고 from일 때 작동하는 이유는 뭘까?
+ * fill은 얕은 복사로 동일한 값을 배열에 적용한다. 그래서 fill을 통해 다차원 배열을 만들려고 하는 경우
+ * 그 배열들이 동일한 곳을 가리키기 때문에 문제가 발생하는 것이었다
+ * 그래서 다차원 배열을 만들고자 할 때는 from을 사용해야 한다.
  */
